a3608a498bf1b0b43b7f2a334a2ddef4a9d12bd6
FFixit <felix-fluegel@web.de>
2022-06-12 21:58:29 +0200

    switches from cra to vite

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,6 @@
 import "./index.css";
-import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 import StartPage from "./startpage/StartPage";
 import GameCreation from "./game/creation/GameCreation";
 import GamePage from "./game/gameview/GamePage";
@@ -28,8 +26,3 @@ render(
 	</BrowserRouter>,
 	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ frontend/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+import tscPlugin from "vite-plugin-tsc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	// This changes the out put dir from dist to build
+	// comment this out if that isn't relevant for your project
+	build: {
+		outDir: "build",
+	},
+	plugins: [
+		tscPlugin(),
+		react(),
+		svgrPlugin({
+			svgrOptions: {
+				icon: true,
+				// ...svgr options (https://react-svgr.com/docs/options/)
+			},
+		}),
+	],
+	server: {
+		proxy: {
+			"/socket.io": {
+				target: "ws://localhost:8080",
+				changeOrigin: true,
+				ws: true,
+			},
+		},
+	},
+});
