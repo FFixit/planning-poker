3e4652b3f289802129cca32c0ba31a679f75489e
FFixit <felix-fluegel@web.de>
2022-07-16 11:54:52 +0200

    fixes frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,30 +4,41 @@ import svgrPlugin from "vite-plugin-svgr";
 import tscPlugin from "vite-plugin-tsc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	// This changes the out put dir from dist to build
-	// comment this out if that isn't relevant for your project
-	build: {
-		outDir: "build",
-	},
-	plugins: [
-		tscPlugin({ tscCommand: "tsc --watch --preserveWatchOutput" }),
-		react(),
-		svgrPlugin({
-			svgrOptions: {
-				icon: true,
-				// ...svgr options (https://react-svgr.com/docs/options/)
-			},
-		}),
-	],
-	server: {
-		proxy: {
-			"/socket.io": {
-				target: "ws://localhost:8080",
-				changeOrigin: true,
-				ws: true,
+
+export default defineConfig(({ command, mode }) => {
+	const configBase = {
+		// This changes the out put dir from dist to build
+		// comment this out if that isn't relevant for your project
+		build: {
+			outDir: "build",
+		},
+		plugins: [
+			react(),
+			svgrPlugin({
+				svgrOptions: {
+					icon: true,
+					// ...svgr options (https://react-svgr.com/docs/options/)
+				},
+			}),
+		],
+		server: {
+			proxy: {
+				"/socket.io": {
+					target: "ws://localhost:8080",
+					changeOrigin: true,
+					ws: true,
+				},
 			},
 		},
-	},
-	clearScreen: false,
+		clearScreen: false,
+	};
+	if (command === "serve") {
+		configBase.plugins.push(
+			tscPlugin({ tscCommand: "tsc --watch --preserveWatchOutput" })
+		);
+		return configBase;
+	} else {
+		// command === 'build'
+		return configBase;
+	}
 });
